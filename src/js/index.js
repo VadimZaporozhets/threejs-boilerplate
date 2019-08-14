4cc7273e57748f34020d0dd63d1b2d2b1aace710
Vadym Zaporozhets <vadym_zaporozhets@epuakyiw0546.kyiv.epam.com>
2019-08-14 19:10:06 +0300

    add common initialization to project

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,127 @@
-import * as THREE from 'three';
+import {
+    WebGLRenderer,
+    PerspectiveCamera,
+    AmbientLight,
+    SpotLight,
+    Scene,
+    PCFSoftShadowMap,
+    SphereGeometry,
+    MeshLambertMaterial,
+    Mesh
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import TWEEN from '@tweenjs/tween.js';
+import Stats from 'stats.js';
+import * as dat from 'dat.gui';
 
-const renderer = new THREE.WebGLRenderer();
+const gui = new dat.GUI();
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    1,
-    500
-);
+const canvas = document.getElementById('canvas');
 
-camera.position.set(0, 0, 100);
+let scene,
+    renderer,
+    camera,
+    stats,
+    controls,
+    screenWidth = window.innerWidth,
+    screenHeight = window.innerHeight;
 
-camera.lookAt(0, 0, 0);
+const createScene = () => {
+    renderer = new WebGLRenderer({
+        antialias: true
+    });
+    scene = new Scene();
+    stats = new Stats();
+    camera = new PerspectiveCamera(75, screenWidth / screenHeight, 0.1, 10000);
+    controls = new OrbitControls(camera, renderer.domElement);
 
-const material = new THREE.LineBasicMaterial({ color: 0x00000ff });
-const geometry = new THREE.Geometry();
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = PCFSoftShadowMap;
 
-geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+    camera.position.set(0, 0, 12);
+    camera.lookAt(0, 0, 0);
 
-const line = new THREE.Line(geometry, material);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    canvas.appendChild(renderer.domElement);
+    document.body.appendChild(stats.dom);
+};
 
-scene.add(line);
+let ambientLight, spotLight;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-renderer.render(scene, camera);
+const createLights = () => {
+    ambientLight = new AmbientLight(0xffffff, 0.3);
+    spotLight = new SpotLight(0xffffff, 1);
 
-// const animate = () => {
-//     requestAnimationFrame(animate);
-// };
-//
-// animate();
+    spotLight.position.set(14, 14, 16);
+    spotLight.angle = Math.PI / 4;
+    spotLight.penumbra = 0.05;
+    spotLight.decay = 2;
+    spotLight.distance = 200;
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = 2048;
+    spotLight.shadow.mapSize.height = 2048;
+    spotLight.shadow.camera.near = 10;
+    spotLight.shadow.camera.far = 200;
+
+    gui.add(spotLight, 'intensity', 0, 2)
+        .name('Spotlight intensity')
+        .listen();
+
+    gui.add(ambientLight, 'intensity', 0, 2)
+        .name('Ambient intensity')
+        .listen();
+
+    scene.add(spotLight);
+    scene.add(ambientLight);
+};
+
+let sphere;
+
+const createObject = () => {
+    const geometry = new SphereGeometry(5, 60, 60);
+    const material = new MeshLambertMaterial({
+        color: 'yellow'
+    });
+
+    sphere = new Mesh(geometry, material);
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
+
+    scene.add(sphere);
+};
+
+const animateObject = () => {
+    sphere.rotation.y += 0.01;
+};
+
+const init = () => {
+    createScene();
+    createLights();
+
+    createObject();
+
+    window.addEventListener('resize', handleWindowResize);
+
+    render();
+};
+
+const render = () => {
+    renderer.render(scene, camera);
+    controls.update();
+    stats.update();
+    TWEEN.update();
+
+    animateObject();
+
+    requestAnimationFrame(render);
+};
+
+const handleWindowResize = () => {
+    screenHeight = window.innerHeight;
+    screenWidth = window.innerWidth;
+    renderer.setSize(screenWidth, screenHeight);
+    camera.aspect = screenWidth / screenHeight;
+    camera.updateProjectionMatrix();
+};
+
+init();
